a4300987a1a3d03ce2a18bbdf12e1e75ada1060d
jjhfrost <hfrost@163.com>
2015-05-25 15:39:50 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,9 @@
-
 #1.Merges the training and the test sets to create one data set.
 #2.Extracts only the measurements on the mean and standard deviation for each measurement. 
 #3.Uses descriptive activity names to name the activities in the data set
 #4.Appropriately labels the data set with descriptive variable names. 
 #5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
 setwd("E:/Coursera/getdata-1")
 
 activityLabel <-read.table(file = "UCI HAR Dataset/activity_labels.txt",header = FALSE,col.names = c("activity","activityLabel"))
@@ -33,7 +33,6 @@ test<-cbind(test,std=apply(temp, 1 ,sd))
 
 #merge train and test set
 all <- rbind(train, test)
-secondDataSet <- all[1,]
 secondDataSet <- data.frame()
 subjectActivity <- unique(all[,1:3])
 
